f2a3bc2fd1312815b2a0aeee312acff52f6a46e1
Liana Veldiaeva <lianali108@gmail.com>
2020-07-18 00:48:57 +0200

    tests added and verifaeyd

---
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+    @Test
+    void findMaxFirstPosition() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxSecondPosition () {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
+
+
+
